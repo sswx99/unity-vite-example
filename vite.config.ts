a6d6e2d3f2da18aaad0137e07380b1aa9959fba3
Serhat S <ss99@tuta.io>
2022-03-03 01:49:08 +0300

    Initial Commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import fs from "fs";
+
+export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync('./.cert/key.pem'),
+      cert: fs.readFileSync('./.cert/cert.pem'),
+    },
+  }
+})
+
